499f6779e93391cb51b64d9861338b3bf50606ab
Angel Delgado <166195230+Delgadoes@users.noreply.github.com>
2025-09-27 14:16:07 -0600

    Update ItemCarrito.kt

--- src/ItemCarrito.kt
+++ src/ItemCarrito.kt
@@ -1,3 +1,20 @@
+/**
+ * Clase ItemCarrito
+ *
+ * Representa un ítem dentro del carrito de compras.
+ * Cada ítem relaciona un producto específico con la cantidad
+ * seleccionada por el usuario.
+ *
+ * Es una clase de tipo "data class" ya que su objetivo principal
+ * es almacenar datos y aprovechar funciones automáticas como:
+ * - equals()
+ * - hashCode()
+ * - toString()
+ * - copy()
+ *
+ * @property producto El producto que el usuario agregó al carrito.
+ * @property cantidad Número de unidades de ese producto.
+ */
 data class ItemCarrito(
     val producto: Producto,
     var cantidad: Int
